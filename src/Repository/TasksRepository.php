fe0d81f0e1150805977a5f8db5d8821d6068a0c6
Paul Richard <paulrpm@gmail.com>
2023-03-17 17:16:25 -0300

    added repositories

---
+++ src/Repository/TasksRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Tasks;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Tasks>
+ *
+ * @method Tasks|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Tasks|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Tasks[]    findAll()
+ * @method Tasks[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class TasksRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Tasks::class);
+    }
+
+    public function save(Tasks $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Tasks $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}
